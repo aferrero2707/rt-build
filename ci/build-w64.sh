8fb508e739704cb5ccc9812d8bf2f0e9ac438e9e
aferrero2707 <aferrero1975@gmail.com>
2018-02-18 20:06:33 +0100

    Disabled installation of InnoSetup configuration

--- ci/build-w64.sh
+++ ci/build-w64.sh
@@ -7,6 +7,8 @@ echo "Compiling RawTherapee"
 ls /sources
 sed -i 's/Shlobj.h/shlobj.h/g' /sources/rtgui/options.cc
 sed -i 's/Shlwapi.h/shlwapi.h/g' /sources/rtgui/placesbrowser.cc
+sed -i 's|install(FILES "${CMAKE_CURRENT_BINARY_DIR}/WindowsInnoSetup.iss"|#install(FILES "${CMAKE_CURRENT_BINARY_DIR}/WindowsInnoSetup.iss"|' /sources/rtdata/CMakeLists.txt
+
 (crossroad cmake -DCMAKE_BUILD_TYPE=Release -DPROC_TARGET_NUMBER=1 \
  -DCACHE_NAME_SUFFIX="'5-dev'" \
  -DCMAKE_C_FLAGS="'-mwin32 -m64 -mthreads -msse2'" \
@@ -15,7 +17,7 @@ sed -i 's/Shlwapi.h/shlwapi.h/g' /sources/rtgui/placesbrowser.cc
  -DCMAKE_CXX_FLAGS_RELEASE="'-Wno-aggressive-loop-optimizations -DNDEBUG -g -O3'" \
  -DCMAKE_EXE_LINKER_FLAGS="'-m64 -mthreads -static-libgcc'" \
  -DCMAKE_EXE_LINKER_FLAGS_RELEASE="'-s -O3'" \
- /sources && make -j 2 && make install) || exit 1
+ /sources && make -j 2 && make install) || rm -rf Release
 #(crossroad cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DPROC_TARGET_NUMBER=1 \
 # -DCACHE_NAME_SUFFIX="'5-dev'" \
 # -DCMAKE_C_FLAGS="'-mwin32 -m64 -mthreads -msse2'" \
